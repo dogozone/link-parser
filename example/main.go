317dfea4d107e04231d9ae9d18d2f4b524b5e961
Darius Pirvulescu <organicdarius@gmail.com>
2023-02-26 19:31:39 +0000

    Create link package, move main.go to example

---
+++ example/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	s := `<html>
+	<body>
+		<h1>Hello!</h1>
+		<a href="/dog">
+						<span>Something in a span</span>
+						Text not in a span
+						<!-- nNOT INCL -->
+						<b>Bold text!</b>
+					</a>
+		<a href="/other-page">A link to another page</a>
+	</body>
+	</html>
+	`
+	doc, err := html.Parse(strings.NewReader(s))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Parse(doc)
+	// dfs(doc, " ")
+	links := Parse(doc)
+	fmt.Printf("final %v\n", links)
+}
--- link.go
+++ link.go
@@ -1,43 +1,16 @@
-package main
+package linkParser
 
 import (
-	"fmt"
-	"log"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
-func main() {
-	s := `<html>
-	<body>
-		<h1>Hello!</h1>
-		<a href="/dog">
-						<span>Something in a span</span>
-						Text not in a span
-						<!-- nNOT INCL -->
-						<b>Bold text!</b>
-					</a>
-		<a href="/other-page">A link to another page</a>
-	</body>
-	</html>
-	`
-	doc, err := html.Parse(strings.NewReader(s))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	parseNode(doc)
-	dfs(doc, " ")
-	links := parseNode(doc)
-	fmt.Printf("final %v\n", links)
-}
-
 type Link struct {
 	Href, Text string
 }
 
-func parseNode(doc *html.Node) []Link {
+func Parse(doc *html.Node) []Link {
 	var links []Link
 	var f func(*html.Node)
 	f = func(n *html.Node) {
@@ -106,10 +79,10 @@ func extractText(ahref *html.Node) []string {
 }
 
 // Basic Depth-first search
-func dfs(n *html.Node, padding string) {
-	fmt.Println(padding, n.Data)
+// func dfs(n *html.Node, padding string) {
+// 	fmt.Println(padding, n.Data)
 
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		dfs(c, padding+"  ")
-	}
-}
+// 	for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 		dfs(c, padding+"  ")
+// 	}
+// }
